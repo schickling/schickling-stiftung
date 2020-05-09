2a15237af9a51c94db0fb70d028167b7788384a3
schickling <schickling.j@gmail.com>
2020-05-09 22:54:59 +0200

    padding

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,8 +2,10 @@ import Layout from '../components/Layout'
 
 const IndexPage = () => (
   <Layout title="Home | Next.js + TypeScript Example">
-    <img src='https://i.imgur.com/076mjd8.png' />
-    <div className='h-16'></div>
+    <div className="py-3">
+      <img src="https://i.imgur.com/076mjd8.png" />
+      <div className="h-16"></div>
+    </div>
   </Layout>
 )
 
