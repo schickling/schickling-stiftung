ed9c40362850cae76d181da763b4c42743ecc0ca
Johannes Schickling <schickling.j@gmail.com>
2020-12-30 16:54:14 +0100

    Flexible layout

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -11,22 +11,32 @@ type Props = {
 const Layout: FC<Props> = ({ children, title }) => {
   const router = useRouter()
   return (
-    <div
-      className="container mx-auto"
-      style={{ maxWidth: 1048, padding: '0 24px' }}
-    >
+    <div>
       <Head>
         <title>{`${title} - Schickling Stiftung`}</title>
         <meta charSet="utf-8" />
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
-      <Topnav currentPath={router.pathname as any} />
+      <Container>
+        <Topnav currentPath={router.pathname as any} />
+      </Container>
       {children}
-      <footer className="py-3 mt-8 font-medium text-gray-700 border-t border-gray-700">
-        <span>Erich Schickling Stiftung © 1998 - 2020</span>
-      </footer>
+      <Container>
+        <footer className="py-3 mt-8 font-medium text-gray-700 border-t border-gray-700">
+          <span>Erich Schickling Stiftung © 1998 - 2020</span>
+        </footer>
+      </Container>
     </div>
-  );
+  )
 }
+
+const Container: FC = ({ children }) => (
+  <div
+    className="container mx-auto"
+    style={{ maxWidth: 1048, padding: '0 24px' }}
+  >
+    {children}
+  </div>
+)
 
 export default Layout
