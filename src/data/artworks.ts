3cec4466a5fe727b87209751804026ca22a38354
Johannes Schickling <schickling.j@gmail.com>
2021-01-01 20:13:31 +0100

    Optimized images

--- src/data/artworks.ts
+++ src/data/artworks.ts
@@ -4,6 +4,7 @@ export type Artwork = {
   title: string
   text: string
   imageUrl: string
+  imageRatio: number
   format: 'Vertikal' | 'Horizontal'
   technique: string
   date: string
@@ -20,6 +21,7 @@ export const artworks: Artwork[] = [
   Even more text`,
     imageUrl:
       'https://www.notion.so/image/https%3A%2F%2Fs3-us-west-2.amazonaws.com%2Fsecure.notion-static.com%2Fd16704b8-12b6-4d4e-8e9d-9d336e619167%2Fkapellenfenster.png?table=block&id=f236df8f-9187-41d8-879f-bc315893491d&width=600&userId=9374aa42-e837-486d-95ad-54573072f4c0&cache=v2',
+    imageRatio: 1.5324947589098532,
     format: 'Vertikal',
     date: 'now',
     technique: 'some',
@@ -34,7 +36,10 @@ export const artworks: Artwork[] = [
     
     Hallo`,
     imageUrl:
-      'https://www.notion.so/image/https%3A%2F%2Fs3-us-west-2.amazonaws.com%2Fsecure.notion-static.com%2Fd930dc43-6e28-4702-9194-d383e58d129c%2Fguenz.jpg?table=block&id=87019bbe-45cf-4a46-b098-d7d6fdac8276&width=600&userId=9374aa42-e837-486d-95ad-54573072f4c0&cache=v2',
+      'https://www.schickling-stiftung.de/static/g-gruene-guenzlandschaft-15274f111b356e79735063f1077e5027.jpg',
+    imageRatio: 0.7042389210019268,
+    // imageUrl:
+    //   'https://www.notion.so/image/https%3A%2F%2Fs3-us-west-2.amazonaws.com%2Fsecure.notion-static.com%2Fd930dc43-6e28-4702-9194-d383e58d129c%2Fguenz.jpg?table=block&id=87019bbe-45cf-4a46-b098-d7d6fdac8276&width=600&userId=9374aa42-e837-486d-95ad-54573072f4c0&cache=v2',
     format: 'Horizontal',
     date: 'now',
     technique: 'some',
@@ -50,6 +55,7 @@ export const artworks: Artwork[] = [
     Hallo`,
     imageUrl:
       'https://www.notion.so/image/https%3A%2F%2Fs3-us-west-2.amazonaws.com%2Fsecure.notion-static.com%2F1021a113-df87-4565-bfd1-e73410965464%2Fengel.jpg?table=block&id=c119a5d3-dc2b-421a-96f6-2da42727f6fd&width=600&userId=9374aa42-e837-486d-95ad-54573072f4c0&cache=v2',
+    imageRatio: 0.6700274977085243,
     format: 'Horizontal',
     date: 'now',
     technique: 'some',
@@ -65,6 +71,7 @@ export const artworks: Artwork[] = [
     Hallo`,
     imageUrl:
       'https://www.notion.so/image/https%3A%2F%2Fs3-us-west-2.amazonaws.com%2Fsecure.notion-static.com%2F1021a113-df87-4565-bfd1-e73410965464%2Fengel.jpg?table=block&id=c119a5d3-dc2b-421a-96f6-2da42727f6fd&width=600&userId=9374aa42-e837-486d-95ad-54573072f4c0&cache=v2',
+    imageRatio: 0.6700274977085243,
     format: 'Horizontal',
     date: 'now',
     technique: 'some',
