84bc3499813623b810121cbc659ffb0718da90a6
Eva-Maria Thuerauf <evamarthuerauf@gmail.com>
2021-01-03 12:47:11 +0100

    wip

--- src/components/ContactBox.tsx
+++ src/components/ContactBox.tsx
@@ -12,10 +12,10 @@ export const ContactBox: FC<{
       <div className="mb-4 text-2xl font-bold">{title}</div>
       <div className="font-normal text-gray-600 mb-9">{description}</div>
       <div className="flex flex-col items-start md:flex-row md:items-center">
-        <div className="px-5 py-3 mb-4 text-lg font-medium text-white bg-black rounded md:mr-5">
+        <div className="px-5 py-3 text-lg font-medium text-white bg-black rounded md:mr-5">
           {button}
         </div>
-        <div className="font-bold text-gray-600">{number}</div>
+        <div className="mt-4 font-bold text-gray-600 md:mt-0">{number}</div>
       </div>
     </div>
   </div>
